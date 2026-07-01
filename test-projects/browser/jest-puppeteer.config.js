92b2f5160f0da9ab050c07bbffed14e80bf9ad38
Jonathan Merklin <jonathan.merklin@hoa-express.com>
2026-07-01 16:03:04 -0500

    VERIFY variant 1: --disable-setuid-sandbox only (keep namespace sandbox)

--- test-projects/browser/jest-puppeteer.config.js
+++ test-projects/browser/jest-puppeteer.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	launch: {
 		executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+		args: ['--disable-setuid-sandbox'],
 	},
 };
